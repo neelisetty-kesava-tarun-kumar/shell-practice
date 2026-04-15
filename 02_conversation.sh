83faa2d062222f2752568d6f9beec2c39dd35497
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-15 19:38:41 +0530

    actions

---
+++ 02_conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "India :: Hello Russia, How are you ?"
+echo "Russia :: I am fine, How is going on in India?"
+echo "India :: No bad, Still the war goes on between the countries"
+echo "Russia :: Yech, I am came to know the situation is getting worse than ever"
