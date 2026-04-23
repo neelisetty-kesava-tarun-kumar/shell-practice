dcf11862f9ea02e49e7b50e1f1469587a49ddcfc
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-23 17:48:29 +0530

    actions

---
+++ 05_variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+echo "Please enter your UserName ::"
+read USER_NAME
+
+echo "User name is $USER_NAME"
+
+Person1=$1 #No Space should between the variable and value
+Person2=$2
+
+
+echo "$Person1 :: Hello $Person2, How are you ?"
+echo "$Person2 :: I am fine $Person1, How is going on in $Person1?"
+echo "$Person1 :: No bad, Still the war goes on between the countries"
+echo "$Person2 :: Yech, I am came to know the situation is getting worse than ever"
