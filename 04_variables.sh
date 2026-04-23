0cc0240a7f3a90f39919964de7bc0e1cd366945e
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-23 17:43:18 +0530

    actions

---
+++ 04_variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+Person1=$1 #No Space should between the variable and value
+Person2=$2
+
+
+echo "$Person1 :: Hello $Person2, How are you ?"
+echo "$Person2 :: I am fine $Person1, How is going on in $Person1?"
+echo "$Person1 :: No bad, Still the war goes on between the countries"
+echo "$Person2 :: Yech, I am came to know the situation is getting worse than ever"
