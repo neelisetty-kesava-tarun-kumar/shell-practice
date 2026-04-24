64fe21f6bea7acb73a2c57812088cf504f5b3a7b
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-24 18:01:12 +0530

    actions

--- 08_datatypes.sh
+++ 08_datatypes.sh
@@ -13,3 +13,6 @@ echo "Sum is :: $SUM"
 FRUITS=("Apple" "Banana" "Papaya")
 
 echo "Fruits are ${FRUITS[@]}" #@ means everything (will be printed by denoting the @ symbol)
+echo "First fruit is ${FRUITS[0]}"
+echo "Second fruit is ${FRUITS[1]}"
+echo "Third fruit is ${FRUITS[2]}"
