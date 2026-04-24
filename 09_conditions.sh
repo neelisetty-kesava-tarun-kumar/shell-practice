72150ff29ff6afaf0c3abdb520807e1d2f8ddb2b
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-24 18:16:58 +0530

    actions

---
+++ 09_conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+NUM1=25
+
+# -gt => greater than
+# -lt => less than 
+# -eq => equal 
+# -ne => not equal
+
+if [$NUM1 -gt 20] ; then
+    echo "Given number $NUM1 is greater than 20"
+fi
