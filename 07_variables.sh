6c2361d1cefe5e269f99fcdb932cc6f99da107f8
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-24 17:08:57 +0530

    actions

---
+++ 07_variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+### Speical Variables ###
+
+echo "All arguments passed to script :: $@"
+echo "Number of variables passed through the script :: $#"
+echo "Script name :: $0"
+echo "Present Directory :: $PWD"
+echo "Who is running :: $USER"
+echo "Home directory of current user :: $HOME"
+echo "PID of the script :: $$"
+sleep 10 &
+echo "PID of recently executed background command :: $!"
+echo "All arguments passed to script :: $*"
