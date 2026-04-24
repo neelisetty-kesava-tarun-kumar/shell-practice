b8d814d9248f3fdf3e2a173011bd9e0caf1e6aeb
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-24 16:52:49 +0530

    actions

--- 06_variables.sh
+++ 06_variables.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 
-echo "Script executed at:: $TIMESTAMP"
+echo "Script executed at:: $START_TIME"
+
+sleep 10 
+
+END_TIME=$(date +%s)
+
+TOTaL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Script executd in:: $TOTaL_TIME"
