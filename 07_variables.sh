ca20eb01324e7965dcfaf47407a3a307c80f34ae
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-24 17:47:48 +0530

    actions

--- 07_variables.sh
+++ 07_variables.sh
@@ -9,6 +9,6 @@ echo "Present Directory :: $PWD"
 echo "Who is running :: $USER"
 echo "Home directory of current user :: $HOME"
 echo "PID of the script :: $$"
-sleep 1     &
+sleep 1 &
 echo "PID of recently executed background command :: $!"
 echo "All arguments passed to script :: $*"
