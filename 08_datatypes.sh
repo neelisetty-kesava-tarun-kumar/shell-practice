3b79536c845f982778bea9c908d8e67bed0d96b3
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-24 17:55:13 +0530

    actions

--- 08_datatypes.sh
+++ 08_datatypes.sh
@@ -1,9 +1,15 @@
 #!/bin/bash 
 
+#INTEGER FORMAT
 NUM1=100
 NUM2=200
 
 
-SUM=$((NUM1+NUM2))
+SUM=$(($NUM1+$NUM2))
 
-echo "$SUM"
+echo "Sum is :: $SUM"
+
+#STRING FORMAT
+FRUITS=("Apple" "Banana" "Papaya")
+
+echo "Fruits are ${FRUITS[@]}" #@ means everything (will be printed by denoting the @ symbol)
