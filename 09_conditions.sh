0c92fc87635bbe9507d33df04ee3bd21f5029818
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-24 18:23:27 +0530

    actions

--- 09_conditions.sh
+++ 09_conditions.sh
@@ -9,6 +9,8 @@ NUM1=$1
 
 if [ $NUM1 -gt 20 ]; then
     echo "Given number $NUM1 is greater than 20"
+elif [ $NUM1 -lt 20 ]; then
+    echo "Given number $NUM1 is less than to 20" 
 else
-    echo "Given number $NUM1 is less than or equal to 20" 
+    echo "Given number $NUM1 is equal to 20" 
 fi
