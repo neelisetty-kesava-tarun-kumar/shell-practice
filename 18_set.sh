1f2bc9016897d2ddd2dd0eae951baa072810fb58
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-25 19:39:13 +0530

    actions

---
+++ 18_set.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e # If any error occurs in the script, it will exit immediately with the error code of the failed command.
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1 
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@
+do
+    dnf list installed $package &>> $LOGS_FILE
+    if [ $? -ne 0 ];then
+        echo "$package is not installed, installing now"
+        dnf install $package -y &>> $LOGS_FILE
+        #VALIDATE $? "$package installation"
+    else
+        echo "$package is already installed, skipping installation process"
+    fi
+done
