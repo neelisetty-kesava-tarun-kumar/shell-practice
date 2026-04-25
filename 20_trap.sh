acb8fa54345be6e117538b84a96ee8be453418d6
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-25 19:58:22 +0530

    actions

--- 20_trap.sh
+++ 20_trap.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-set -e #ERROR
-trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERROR
+set -e
+trap 'echo "Error at line $LINENO: $BASH_COMMAND"' ERR
 
 USERID=$(id -u)
+
 LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="/var/log/shell-script/$0.log"
+mkdir -p $LOGS_FOLDER
+
+LOGS_FILE="$LOGS_FOLDER/$(basename $0).log"
+
+# Colors
 R='\033[0;31m'
 G='\033[0;32m'
 Y='\033[0;33m'
-B='\033[0;34m'
-M='\033[0;35m'
-C='\033[0;36m'
-W='\033[0;37m'
+N='\033[0m'
 
-if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+if [ "$USERID" -ne 0 ]; then
+    echo -e "${R}Please run this script with root user access${N}" | tee -a "$LOGS_FILE"
     exit 1 
 fi
 
-mkdir -p $LOGS_FOLDER
-
-for package in $@
+for package in "$@"
 do
-    dnf list installed $package &>> $LOGS_FILE
-    if [ $? -ne 0 ];then
-        echo "$package is not installed, installing now"
-        dnf install $package -y &>> $LOGS_FILE
-        
+    if ! dnf list installed "$package" &>> "$LOGS_FILE"; then
+        echo "$package is not installed, installing now" | tee -a "$LOGS_FILE"
+        dnf install "$package" -y &>> "$LOGS_FILE"
+        echo -e "${G}$package installation SUCCESS${N}" | tee -a "$LOGS_FILE"
     else
-        echo -e "$G $package is already installed, $Y skipping installation process $N"
+        echo -e "${Y}$package already installed, skipping${N}" | tee -a "$LOGS_FILE"
     fi
 done
