473f2afdab406eee4f78acdae5d68a30edda7a22
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-25 17:43:47 +0530

    actions

--- 10_installation.sh
+++ 10_installation.sh
@@ -11,8 +11,26 @@ echo "Installing Nginx"
 dnf install nginx -y
 
 if [ $? -ne 0 ]; then
-    echo "Installing Nginx .. FAILED"
+    echo "Installing Nginx ... FAILURE"
     exit 1
 else
-    echo "Installing Nginx .. SUCCESS"
+    echo "Installing Nginx ... SUCCESS"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing MySQL ... FAILURE"
+    exit 1
+else
+    echo "Installing MySQL ... SUCCESS"
+fi
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing nodejs ... FAILURE"
+    exit 1
+else
+    echo "Installing nodejs ... SUCCESS"
 fi
