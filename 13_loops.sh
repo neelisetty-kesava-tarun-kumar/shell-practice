85821b05642b94cc40c18d2672d0ef5ba0ecbef3
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-25 18:48:02 +0530

    actions

--- 13_loops.sh
+++ 13_loops.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#Simple For loop
+#Simple For loop 
 for i in {1..100}
 do 
     echo Number is $i
 done
+
+# The 'i' will read the number from the flower brackets one by one and print in the loop in each line.
