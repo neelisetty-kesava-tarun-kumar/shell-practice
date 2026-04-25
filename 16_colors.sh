29ca0ed39f233737e73ba0ac6d41a142b03cd04a
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-25 19:22:50 +0530

    actions

--- 16_colors.sh
+++ 16_colors.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-R=ED='\033[0;31m'
-G=ED='\033[0;32m'
-Y=ED='\033[0;33m'
-B=ED='\033[0;34m'
-M=ED='\033[0;35m'
-C=ED='\033[0;36m'
-W=ED='\033[0;37m'
+R='\033[0;31m'
+G='\033[0;32m'
+Y='\033[0;33m'
+B='\033[0;34m'
+M='\033[0;35m'
+C='\033[0;36m'
+W='\033[0;37m'
 
 echo -e "${R}This is red color"
 echo -e "${G}This is green color"
@@ -16,4 +16,3 @@ echo -e "${M}This is magenta color"
 echo -e "${C}This is cyan color"
 echo -e "${W}This is white color"
 
-echo -e "${R}This is red color ${G}This is green color ${Y}This is yellow color ${B}This is blue color ${M}This is magenta color ${C}This is cyan color ${W}This is white color"
