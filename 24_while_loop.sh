ddc798505f2d8c42fd2106490ff8aabe75865ae0
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-05-07 20:48:50 +0530

    actions

--- 24_while_loop.sh
+++ 24_while_loop.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-count=1
-while [ $count -le 5 ]; do
-    echo "Counter: $count"
-    ((count++))
-done
+# count=1
+# while [ $count -le 5 ]; do
+#     echo "Counter: $count"
+#     ((count++))
+# done
+
+echo "While loop has completed."
+
+while IFS= read -r line; do
+    echo "Read line: $line"
+done < ./21_read_file.sh #Give the input file name to read.
