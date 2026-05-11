f2c56cb7a6fd11f92f4e9e49cf405994f265e6af
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-05-11 16:18:20 +0530

    actions

--- 25_backup.sh
+++ 25_backup.sh
@@ -8,6 +8,9 @@ R='\033[0;31m'
 G='\033[0;32m'
 Y='\033[0;33m'
 W='\033[0;37m'
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} #14 days is the default value if user is not provided.
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" 
@@ -22,4 +25,14 @@ USAGE(){
 
 if [ $# -lt 2 ]; then
     USAGE
+fi
+
+if [ -d $SOURCE_DIR ]: then
+    echo -e "$G Source directory $SOURCE_DIR not exists. $N"
+    exit 1
+fi
+
+if [ -d $DEST_DIR ]: then
+    echo -e "$G Destination directory $DEST_DIR not exists. $N"
+    exit 1
 fi
