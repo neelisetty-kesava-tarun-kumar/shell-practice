c4ff9a52972052852dd1b6b34ee5f02230ce60d4
NEELISETTY KESAVA TARUN KUMAR <2200030017cseh@gmail.com>
2026-05-11 15:39:27 +0530

    Update 23_delete_old_logs.sh

--- 23_delete_old_logs.sh
+++ 23_delete_old_logs.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-R='\033[0;31m'
-G='\033[0;32m'
-Y='\033[0;33m'
-B='\033[0;34m'
-M='\033[0;35m'
-C='\033[0;36m'
-W='\033[0;37m'
-N='\033[0m'
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-LOG_DIR="/home/ec2-user/app-logs" #Changes according to the user
-LOG_FILE="$LOG_DIR/$0.log"
+LOGS_DIR=/home/ec2-user/app-logs
+LOGS_FILE="$LOGS_DIR/$(basename "$0").log"
 
-if [ ! -d "$LOG_DIR" ]; then
-    echo -e "${Y}Log directory does not exist. Creating it...${N}"
+if [ ! -d $LOGS_DIR ]; then
+    echo -e "$LOGS_DIR does not exitst"
     exit 1
 fi
 
-FILES_TO_DELETE=$(find "$LOG_DIR" -type f -name "*.log" -mtime +7)
-#echo "$FILES_TO_DELETE"
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
+# echo "$FILES_TO_DELETE"
 
-
-while IFS= read -r filepath; do
-    echo "Deleting file: $filepath"
-    rm -f "$filepath"
-    echo "Deleted file: $filepath"
-done <<< "$FILES_TO_DELETE" 
+if [ -z "$FILES_TO_DELETE" ]; then
+    echo "No files older than 14 days to delete."
+  else
+    while IFS= read -r filepath; do
+      # Process each line here
+      echo "Deleting file: $filepath"
+      rm -f $filepath
+      echo "Deleted file: $filepath"
+    done <<< $FILES_TO_DELETE
+fi
