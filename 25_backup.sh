7ee8875760c2c2ac2193b0d0076059999c9d91df
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-05-11 16:07:25 +0530

    actions

---
+++ 25_backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/backup.log"
+R='\033[0;31m'
+G='\033[0;32m'
+Y='\033[0;33m'
+W='\033[0;37m'
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" 
+fi
+
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e "$R Usage: $0 <source_directory> <destination_directory> [default 14 days]$N"
+    exit 1
+}
+
+if [ $# -lt 2 ]; then
+    USAGE
+fi
