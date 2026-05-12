fd7f75a962f09185deb1f6bef054e91c12718f01
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-05-12 11:42:59 +0530

    actions

--- mail.sh
+++ mail.sh
@@ -7,7 +7,7 @@ SERVER_IP="$5"
 DevOps_Team="$6"
 FINAL_MESSAGE_BODY=$(echo "$MESSAGE_BODY" | sed "s/'/'\\\\''/g")
 
-FINAL_MESSAGE=$(sed "s/DevOps_Team/$6/g" -e "s/ALERT_TYPE/$4/g" -e "s/SERVER_IP/$5/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g"  mail.html)
+FINAL_MESSAGE=$(sed "s/DevOps_Team/$DevOps_Team/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g"  mail.html)
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
