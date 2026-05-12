5ccf80d9d6f8401cde4d6e2e2e65d9e1c8c66ab9
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-05-12 13:19:27 +0530

    actions

--- mail.sh
+++ mail.sh
@@ -6,9 +6,20 @@ MESSAGE_BODY="$3"
 ALERT_TYPE="$4"
 SERVER_IP="$5"
 TO_TEAM="$6"
-FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed "s/'/'\\\\''/g")
+#FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed "s/'/'\\\\''/g")
 
-FINAL_MESSAGE=$(sed "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" mail.html)
+#FINAL_MESSAGE=$(sed "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" mail.html)
+
+# Read HTML file
+FINAL_MESSAGE=$(cat mail.html)
+
+# Replace simple placeholders
+FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | sed "s|TO_TEAM|$TO_TEAM|g")
+FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | sed "s|ALERT_TYPE|$ALERT_TYPE|g")
+FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | sed "s|SERVER_IP|$SERVER_IP|g")
+
+# Replace MESSAGE safely without sed
+FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | awk -v msg="$MESSAGE_BODY" '{gsub(/MESSAGE/,msg)}1')
 
 # Send mail using msmtp
 {
