73ce94035915a3940b99f7ea2e81c02e7d8bce0d
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-05-12 11:48:21 +0530

    actions

--- 26_disk_usage.sh
+++ 26_disk_usage.sh
@@ -4,29 +4,33 @@ R='\033[0;31m'
 G='\033[0;32m'
 Y='\033[0;33m'
 W='\033[0;37m'
+N='\033[0m'
+
 MESSAGE=""
-IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
 log(){
-    log -e "$(date "+%Y-%m-%d %H:%M:%S") | $1 " | tee -a $LOGS_FILE
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
 }
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-USAGE_THRESHOLD=3 # Set the threshold for disk usage percentage, Generally, 80% or higher is considered high. I am using 3% for testing purpose, you can change it to 80% or higher.
+
+# Generally use 80 or above in production
+USAGE_THRESHOLD=3
 
 while IFS= read -r line
 do
     USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
-    PARTITON=$(echo "$line" | awk '{print $7}')
+    PARTITION=$(echo "$line" | awk '{print $7}')
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE+="$R Disk usage for partition $PARTITON is at $USAGE% which is above the threshold of $USAGE_THRESHOLD% $N \n"
+        MESSAGE+="High Disk usage on $PARTITION : $USAGE% which is above threshold $USAGE_THRESHOLD% <br>"
     else
-        MESSAGE+="$G Disk usage for partition $PARTITON is at $USAGE% which is below the threshold of $USAGE_THRESHOLD% $N \n"
+        MESSAGE+="Disk usage on $PARTITION : $USAGE% which is below threshold $USAGE_THRESHOLD% <br>"
     fi
 
 done <<< "$DISK_USAGE"
 
 echo -e "$MESSAGE"
 
-sh mail.sh "ntarun1894@gmail.com" "High disk usage alert on $IP_ADDRESS" "$MESSAGE" "High Disk Usage Alert" "$IP_ADDRESS" "DevOps Team"
+sh mail.sh \ "ntarun1894@gmail.com" \ "High Disk Usage Alert on $IP_ADDRESS" \ "$MESSAGE" \ "HIGH_DISK_USAGE" \ "$IP_ADDRESS" \ "DevOps Team"
--- mail.sh
+++ mail.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-TO_ADDRESS="$1"
-SUBJECT="$2"
-MESSAGE_BODY="$3"
-ALERT_TYPE="$4"
-SERVER_IP="$5"
-DevOps_Team="$6"
-FINAL_MESSAGE_BODY=$(echo "$MESSAGE_BODY" | sed "s/'/'\\\\''/g")
+TO_ADDRESS=$1
+SUBJECT=$2
+MESSAGE_BODY=$3
+ALERT_TYPE=$4
+SERVER_IP=$5
+TO_TEAM=$6
+FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
-FINAL_MESSAGE=$(sed "s/DevOps_Team/$DevOps_Team/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g"  mail.html)
+FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
+
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
