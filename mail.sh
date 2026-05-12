bf20d0a7b71c2cea485af95e2df91d7ae159a8cf
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-05-12 12:59:12 +0530

    actions

--- mail.sh
+++ mail.sh
@@ -7,12 +7,22 @@ ALERT_TYPE="$4"
 SERVER_IP="$5"
 TO_TEAM="$6"
 
-FINAL_MESSAGE=$(sed "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$MESSAGE_BODY/g" mail.html)
+# Read HTML template
+FINAL_MESSAGE=$(cat mail.html)
 
+# Replace placeholders
+FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | sed "s|TO_TEAM|$TO_TEAM|g")
+FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | sed "s|ALERT_TYPE|$ALERT_TYPE|g")
+FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | sed "s|SERVER_IP|$SERVER_IP|g")
+
+# Replace MESSAGE safely
+FINAL_MESSAGE=${FINAL_MESSAGE//MESSAGE/$MESSAGE_BODY}
+
+# Send mail using msmtp
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
 echo "Content-Type: text/html"
 echo ""
-echo "$FINAL_MESSAGE"
+echo -e "$FINAL_MESSAGE"
 } | msmtp "$TO_ADDRESS"
