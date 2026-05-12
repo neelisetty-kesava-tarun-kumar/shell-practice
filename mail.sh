978ddef10705feb25188bcfb43c224515bf5ca8b
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-05-12 13:16:05 +0530

    actions

--- mail.sh
+++ mail.sh
@@ -6,19 +6,9 @@ MESSAGE_BODY="$3"
 ALERT_TYPE="$4"
 SERVER_IP="$5"
 TO_TEAM="$6"
+FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed "s/'/'\\\\''/g")
 
-# Read HTML template
-#FINAL_MESSAGE=$(cat mail.html)
-
-# Replace placeholders
-#FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | sed "s|TO_TEAM|$TO_TEAM|g")
-#FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | sed "s|ALERT_TYPE|$ALERT_TYPE|g")
-#FINAL_MESSAGE=$(echo "$FINAL_MESSAGE" | sed "s|SERVER_IP|$SERVER_IP|g")
-
-# Replace MESSAGE safely
-#FINAL_MESSAGE=${FINAL_MESSAGE//MESSAGE/$MESSAGE_BODY}
-
-FINAL_MESSAGE=$(sed "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$MESSAGE_BODY/g" mail.html)
+FINAL_MESSAGE=$(sed "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" mail.html)
 
 # Send mail using msmtp
 {
