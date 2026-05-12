be6007aa45255b317adf5f94183375abb7eb41f1
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-05-12 11:41:28 +0530

    actions

--- mail.sh
+++ mail.sh
@@ -5,7 +5,7 @@ MESSAGE_BODY="$3"
 ALERT_TYPE="$4"
 SERVER_IP="$5"
 DevOps_Team="$6"
-FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed "s/'/'\\\''/g")
+FINAL_MESSAGE_BODY=$(echo "$MESSAGE_BODY" | sed "s/'/'\\\\''/g")
 
 FINAL_MESSAGE=$(sed "s/DevOps_Team/$6/g" -e "s/ALERT_TYPE/$4/g" -e "s/SERVER_IP/$5/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g"  mail.html)
 {
